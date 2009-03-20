f964f5696252958a9b162f73b0d25b1a5ace8735
Joe Damato <ice799@gmail.com>
2009-03-20 02:59:12 -0700

    fixing extconf to check for libpthread and pthread.h

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,5 +1,7 @@
 require 'mkmf'
-if ( have_library('c', 'shm_open') or
-   have_library('rt', 'shm_open') )
+if ( (have_library('c', 'shm_open') or
+      have_library('rt', 'shm_open')) and
+     (have_library('pthread', 'pthread_mutex_lock')) and
+     (have_header('pthread.h')))
    create_makefile ('putex')
 end
